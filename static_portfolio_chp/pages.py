b599b3b1738690d6931221e28f62cbe7ed31e2ba
Aibek Seitov <aybekko97@yandex-team.ru>
2021-10-30 08:53:21 +0300

    update static_portfolio_chp

--- static_portfolio_chp/pages.py
+++ static_portfolio_chp/pages.py
@@ -1,11 +1,23 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
+import json
 
 
 class MyPage(Page):
     form_model = 'player'
     form_fields = ['lottery_chp']
+
+    def vars_for_template(self):
+        r = self.round_number
+        p = self.player
+        wealth = json.loads(p.participant.vars["dyn_wealth_round{}".format(r)])
+        return {
+            'w_1': wealth["w_3_1"],
+            'w_2': (wealth["w_3_2"] + wealth["w_3_3"] + wealth["w_3_5"])/3,
+            'w_3': (wealth["w_3_4"] + wealth["w_3_6"] + wealth["w_3_7"])/3,
+            'w_4': wealth["w_3_8"],
+        }
 
 
 class ResultsWaitPage(WaitPage):
