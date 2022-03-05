75e10350c92453de2ea972e23e7a439eb4c70928
askhat-omar <askhat.omar@gmail.com>
2022-03-05 23:00:27 +0600

    changing the way participants are randomly assigned to the treatment

--- introduction/pages.py
+++ introduction/pages.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 from otree.api import Currency as c, currency_range
 
@@ -14,7 +15,17 @@ class Page1(Page):
 
     def app_after_this_page(player, upcoming_apps):
         # generate and initialize step
-        player.participant.vars["app_id"] = random.randrange(len(settings.APP_KITS))
+        iid_probs = np.random.binomial(n = 1, p = 0.5, size = 1)
+        full_first = np.random.binomial(n = 1, p = 0.5, size = 1)
+        if (full_first == 1) & (iid_probs == 1):
+            player.participant.vars["app_id"] = 2
+        elif (full_first == 1) & (iid_probs == 0):
+            player.participant.vars["app_id"] = 0
+        elif (full_first == 0) & (iid_probs == 1):
+            player.participant.vars["app_id"] = 3
+        elif (full_first == 0) & (iid_probs == 0):
+            player.participant.vars["app_id"] = 1
+
         player.participant.vars["step"] = 0
 
         player.participant.vars["step"] += 1
