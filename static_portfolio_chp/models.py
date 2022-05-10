f67661a40476c53fac742b6ad098453b02b7d5a7
askhat-omar <askhat.omar@gmail.com>
2022-05-10 22:53:36 +0600

    changing static_portfolio_chp

--- static_portfolio_chp/models.py
+++ static_portfolio_chp/models.py
@@ -17,13 +17,30 @@ class Constants(BaseConstants):
     name_in_url = 'static_portfolio_chp'
     players_per_group = None
     num_rounds = 1
-    b = [136.71, 123.69, 101.83, 83.72]
+    initial_wealth = 100
+    probabilities = {"pr_1": 0.111, "pr_2": 0.361, "pr_3": 0.403, "pr_4": 0.125}
+    payoff_a = {"pay_1": 136.71, "pay_2": 123.69, "pay_3": 101.83, "pay_4": 83.72}
+    payoff_d = {"pay_1": 300.00, "pay_2": 222.12, "pay_3": 93.43, "pay_4": 42.19}
+    a = [136.71, 123.69, 101.83, 83.72]
     d = [300.00, 222.12, 93.43, 42.19]
-    p = [0.111, 0.361, 0.403, 0.125]
+    probs_for_payoff = [0.111, 0.361, 0.403, 0.125]
+    states_for_payoff = [1, 2, 3, 4]
+
+
 
 
 class Subsession(BaseSubsession):
-    pass
+    num_periods = models.IntegerField()
+
+    def creating_session(self):
+        num_rounds = Constants.num_rounds
+        if self.round_number == 1:
+            self.session.vars['num_rounds'] = num_rounds
+            for r in range(1, num_rounds+1):
+                t = self.session.config['round{}_T'.format(r)]
+                self.in_round(r).num_periods = t
+                self.session.vars["static_num_periods_round{}".format(r)] = self.in_round(r).num_periods
+
 
 
 class Group(BaseGroup):
@@ -31,28 +48,65 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    lottery_chp = models.StringField(
-        choices=[[1,'I prefer asset A (not to invest)'], [2, 'I prefer asset B'], [3, 'I prefer asset C'], [4, 'I prefer asset D']], widget=widgets.RadioSelect
+    lottery = models.StringField(
+        choices=[[1,'I prefer asset A'], [2, 'I prefer asset B'], [3, 'I prefer asset C'], [4, 'I prefer asset D']], widget=widgets.RadioSelect
     )
 
     w_1 = models.StringField()
     w_2 = models.StringField()
     w_3 = models.StringField()
     w_4 = models.StringField()
+    pf_1 = models.StringField()
+    pf_2 = models.StringField()
+    pf_3 = models.StringField()
+    pf_4 = models.StringField()
+    payoff_b = models.StringField()
+    payoff_c = models.StringField()
+    static_realized_state = models.IntegerField()
+    static_realized_pay = models.FloatField()
+    probabilities = models.StringField()
+    payoff_a = models.StringField()
+    payoff_d = models.StringField()
 
-    def set_wealth(self, w_1, w_2, w_3, w_4):
+    def set_wealth(self, w_1, w_2, w_3, w_4, pf_1, pf_2, pf_3, pf_4):
         self.w_1 = w_1
         self.w_2 = w_2
         self.w_3 = w_3
         self.w_4 = w_4
+        self.pf_1 = pf_1
+        self.pf_2 = pf_2
+        self.pf_3 = pf_3
+        self.pf_4 = pf_4
+        asset_b = {"pf_1": self.pf_1, "pf_2": self.pf_2, "pf_3": self.pf_3, "pf_4": self.pf_4}
+        asset_c = {"pay_1": self.w_1, "pay_2": self.w_2, "pay_3": self.w_3, "pay_4": self.w_4}
+        self.payoff_b = json.dumps(asset_b)
+        self.payoff_c = json.dumps(asset_c)
+        self.probabilities = json.dumps(Constants.probabilities)
+        self.payoff_a = json.dumps(Constants.payoff_a)
+        self.payoff_d = json.dumps(Constants.payoff_d)
 
     def for_payoff(self):
-        if self.lottery_chp == '1':
-            k = 100
-        elif self.lottery_chp == '2':
-            k = numpy.random.choice(Constants.b, p=Constants.p)
-        elif self.lottery_chp == '3':
-            k = numpy.random.choice([self.w_1, self.w_2, self.w_3, self.w_4], p=Constants.p)
+        self.static_realized_state = int(numpy.random.choice(Constants.states_for_payoff, p=Constants.probs_for_payoff))
+        portfolio_label = "pf_{}"
+        payoff_label = "pay_{}"
+        asset_b = {"pf_1": self.pf_1, "pf_2": self.pf_2, "pf_3": self.pf_3, "pf_4": self.pf_4}
+        asset_c = {"pay_1": self.w_1, "pay_2": self.w_2, "pay_3": self.w_3, "pay_4": self.w_4}
+        if self.lottery == '1':
+            k = Constants.payoff_a[payoff_label.format(self.static_realized_state)]
+        elif self.lottery == '2':
+            k = asset_b[portfolio_label.format(self.static_realized_state)]
+        elif self.lottery == '3':
+            k = asset_c[payoff_label.format(self.static_realized_state)]
         else:
-            k = numpy.random.choice(Constants.d, p=Constants.p)
-        self.payoff = k
+            k = Constants.payoff_d[payoff_label.format(self.static_realized_state)]
+        self.static_realized_pay = float(k)
+        self.payoff = self.static_realized_pay
+        r = self.round_number
+        self.participant.vars["static_probabilities_round{}".format(r)] = self.probabilities
+        self.participant.vars["static_payoff_a_round{}".format(r)] = self.payoff_a
+        self.participant.vars["static_payoff_d_round{}".format(r)] = self.payoff_d
+        self.participant.vars["static_realized_state_round{}".format(r)] = self.static_realized_state
+        self.participant.vars["static_realized_pay_round{}".format(r)] = self.static_realized_pay
+        self.participant.vars["static_payoff_b_round{}".format(r)] = self.payoff_b
+        self.participant.vars["static_payoff_c_round{}".format(r)] = self.payoff_c
+
--- static_portfolio_chp/pages.py
+++ static_portfolio_chp/pages.py
@@ -7,40 +7,31 @@ import json
 
 FORMAT_FLOAT = "{:.2f}"
 
+
 class MyPage(Page):
     form_model = 'player'
-    form_fields = ['lottery_chp']
+    form_fields = ['lottery']
 
     def vars_for_template(self):
         r = self.round_number
         p = self.player
         wealth = json.loads(p.participant.vars["dyn_wealth_round{}".format(r)])
-        p.set_wealth(FORMAT_FLOAT.format(wealth["w_3_1"]),
-                     FORMAT_FLOAT.format(wealth["w_3_2"] * 0.615 + wealth["w_3_3"] * 0.154 + wealth["w_3_5"] * 0.231),
-                     FORMAT_FLOAT.format(wealth["w_3_4"] * 0.276 + wealth["w_3_6"] * 0.414 + wealth["w_3_7"] * 0.310),
-                     FORMAT_FLOAT.format(wealth["w_3_8"]))
+        portfolio = json.loads(p.participant.vars["newt2_portfolio_round{}".format(r)])
+        p.set_wealth(FORMAT_FLOAT.format(wealth["w_3_1"]), FORMAT_FLOAT.format((wealth["w_3_2"] + wealth["w_3_3"] + wealth["w_3_5"]) / 3), FORMAT_FLOAT.format((wealth["w_3_4"] + wealth["w_3_6"] + wealth["w_3_7"]) / 3), FORMAT_FLOAT.format(wealth["w_3_8"]), FORMAT_FLOAT.format(portfolio["pf_1"]), FORMAT_FLOAT.format(portfolio["pf_2"]), FORMAT_FLOAT.format(portfolio["pf_3"]), FORMAT_FLOAT.format(portfolio["pf_4"]))
         return {
-            'w_1': p.w_1,
-            'w_2': p.w_2,
-            'w_3': p.w_3,
-            'w_4': p.w_4,
+            'num_states': self.subsession.num_periods + 1,
+            'payoff_b': p.payoff_b,
+            'payoff_c': p.payoff_c,
         }
 
     def before_next_page(self):
         p = self.player
         p.for_payoff()
 
-
-
-class ResultsWaitPage(WaitPage):
-    pass
-
-
 class Results(Page):
     def app_after_this_page(player, upcoming_apps):
         player.participant.vars["step"] += 1
         return get_next_app(app_index=player.participant.vars["app_id"],
                             step=player.participant.vars["step"])
-
 
 page_sequence = [MyPage, Results]
