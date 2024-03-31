8fdffcc52cee6e661fe9c21bb8fea9de8a163347
askhat-omar <askhat.omar@gmail.com>
2024-04-01 02:30:50 +0500

    Editing a pooled task

--- tr3_dynamic_pooled/pages.py
+++ tr3_dynamic_pooled/pages.py
@@ -24,9 +24,17 @@ class MyPage(Page):
                             step=player.participant.vars["step"])
 
 
-class ResultsWaitPage(WaitPage):
-    def is_displayed(self):
-        return False
+class Task_1(Page):
+    form_model = 'player'
+    form_fields = ['dyn_stock', 'dyn_bond', 'dyn_wealth', 'fill_history']
+
+    def vars_for_template(self):
+        r = self.round_number
+        p = self.player
+        p.create_prices()
+        return {
+            'dyn_prices': p.participant.vars["dyn_prices_round{}".format(r)]
+        }
 
 
 class Results(Page):
@@ -34,4 +42,4 @@ class Results(Page):
         return self.round_number == 1
 
 
-page_sequence = [Results, MyPage]
+page_sequence = [Results, Task_1, MyPage]
