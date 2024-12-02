f3c438d39c7fecbdd382f74ed1635f027f69d49b
askhat-omar <askhat.omar@gmail.com>
2024-12-02 23:50:30 +0500

    changing names

--- task2_prac_tr13/models.py
+++ task2_prac_tr13/models.py
@@ -14,7 +14,7 @@ Your app description
 
 
 class Constants(BaseConstants):
-    name_in_url = 'new_task2_practice'
+    name_in_url = 'task2_prac_tr13'
     players_per_group = None
     num_rounds = 1
     initial_wealth = 100
