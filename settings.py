9bc4ef0ea4d1454ab136e261766deb8ee69910ed
askhat-omar <askhat.omar@gmail.com>
2024-12-15 15:10:05 +0500

    changing app names

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ APP_KITS = {
             'task3_tr13',
             'task3_results_tr13',
             'task4_tr13',
-            'task4_results',
+            'task4_results_tr13',
             'crt',
             'survey',
             'finalpage'
@@ -75,7 +75,7 @@ APP_KITS = {
             'task3_tr13',
             'task3_results_tr13',
             'task4_tr13',
-            'task4_results',
+            'task4_results_tr13',
             'crt',
             'survey',
             'finalpage'
